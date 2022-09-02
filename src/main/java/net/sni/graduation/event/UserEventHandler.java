a06192cdd79299d32968c0312225ecbfd4b1585a
egurkan <emre-gurkan@hotmail.com>
2022-09-02 18:01:03 +0300

    Removed redundant line from event handler

--- src/main/java/net/sni/graduation/event/UserEventHandler.java
+++ src/main/java/net/sni/graduation/event/UserEventHandler.java
@@ -2,7 +2,6 @@ package net.sni.graduation.event;
 
 import lombok.RequiredArgsConstructor;
 import net.sni.graduation.constant.RoleEnum;
-import net.sni.graduation.entity.Role;
 import net.sni.graduation.entity.User;
 import net.sni.graduation.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 @RepositoryEventHandler
@@ -22,8 +19,7 @@ public class UserEventHandler {
 
     @HandleBeforeCreate
     public void hashUserPassword(User user) {
-        Optional<Role> userRole = roleRepository.findByRole(RoleEnum.USER);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRole.ifPresent(role -> user.getRoles().add(role));
+        roleRepository.findByRole(RoleEnum.USER).ifPresent(role -> user.getRoles().add(role));
     }
 }
