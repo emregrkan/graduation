9cfded99f3cf59e9dc809496ac359e745351feb8
Emre Gürkan <emre-gurkan@hotmail.com>
2022-09-05 00:41:38 +0300

    Removed redundant import from JwtFilter

--- src/main/java/net/sni/graduation/security/JwtFilter.java
+++ src/main/java/net/sni/graduation/security/JwtFilter.java
@@ -9,7 +9,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
